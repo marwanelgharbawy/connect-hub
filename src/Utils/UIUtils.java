d30e22e41e8a4b59246cdd923b4a995851fea853
Marwan <marwanelgharbawy2@gmail.com>
2024-12-03 21:21:49 +0200

    add UI Utils

---
+++ src/Utils/UIUtils.java
@@ -0,0 +1,12 @@
+package Utils;
+
+import javax.swing.*;
+
+public class UIUtils {
+    public static void initializeWindow(JFrame window, JPanel panel, String title, int width, int height) {
+        window.setContentPane(panel);
+        window.setTitle(title);
+        window.setSize(width, height);
+        window.setVisible(true);
+    }
+}
