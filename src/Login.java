af4ff01d339e87bb02c8295962fb4305d9016112
Marwan <marwanelgharbawy2@gmail.com>
2024-12-03 21:38:06 +0200

    add buttons for login

--- src/Login.java
+++ src/Login.java
@@ -1,15 +1,31 @@
+import Utils.UIUtils;
+
 import javax.swing.*;
 
 public class Login extends JFrame {
     private JPanel contentPane;
     private JPasswordField passwordField1;
     private JTextField textField1;
+    private JButton backButton;
+    private JButton loginButton;
 
     public Login() {
-        setContentPane(contentPane);
-        setTitle("Login");
-        setSize(320, 240);
-        setVisible(true);
+        UIUtils.initializeWindow(this, contentPane, "Login", 400, 400);
+        addEventListeners();
+    }
+
+    private void addEventListeners() {
+        loginButton.addActionListener(e -> {
+            String username = textField1.getText();
+            String password = new String(passwordField1.getPassword());
+
+            // TODO: Implement login logic
+        });
+
+        backButton.addActionListener(e -> {
+            new MainMenu();
+            dispose();
+        });
     }
 
     public static void main(String[] args) {
