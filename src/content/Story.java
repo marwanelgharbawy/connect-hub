f9f51339a3988c927ab0a5e0610f74c82e574e1d
Habiba <habibaswaify@hotmail.com>
2024-12-04 19:30:27 +0200

    Add Story.java

---
+++ src/content/Story.java
@@ -0,0 +1,14 @@
+package content;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Story extends CreatedContent{
+    Story(String authorId, String text, String imagePath) {
+        super(authorId, text, imagePath);
+    }
+    // Check if the story is due in order to delete it
+    boolean isDue(){
+        return (LocalDateTime.now().getHour() - super.timestamp.getHour() >= 24);
+    }
+}
