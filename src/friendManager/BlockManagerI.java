8445b80f2b2826b139f4614da8af3fbb0d4380ba
Habiba <habibaswaify@hotmail.com>
2024-12-04 23:06:20 +0200

    Modify BlockManager

--- src/friendManager/BlockManagerI.java
+++ src/friendManager/BlockManagerI.java
@@ -5,7 +5,7 @@ import backend.User;
 import java.util.ArrayList;
 
 public interface BlockManagerI {
-    void blockUser(User userToBlock);
+    void blockUser(User mainUser, User userToBlock);
     void unblockUser(User userToUnblock);
     boolean isBlocked(User user);
     ArrayList<User> getBlockedUsers();
