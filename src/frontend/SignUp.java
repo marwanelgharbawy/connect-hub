ba3f4e8ede928d5aa14879288d847f24449de844
Marwan <marwanelgharbawy2@gmail.com>
2024-12-04 22:22:31 +0200

    add frontend validations

--- src/frontend/SignUp.java
+++ src/frontend/SignUp.java
@@ -1,10 +1,13 @@
 package frontend;
 
+import backend.Database;
 import utils.Utilities;
 import backend.User;
 
 import javax.swing.*;
+import javax.xml.crypto.Data;
 import java.awt.*;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Date;
 
@@ -40,11 +43,39 @@ public class SignUp extends JFrame{
         window.add(signUpButton);
 
         signUpButton.addActionListener(_ -> {
+            // Get data from fields
             String username = usernameField.getText();
             String email = emailField.getText();
+            Date dateOfBirth = (Date) dateSpinner.getValue();
             String password = "";
 
-            // Return if password don't match
+            // Frontend validations
+
+            // Return if any field is empty
+            if (username.isEmpty() || email.isEmpty() || Arrays.toString(passwordField.getPassword()).isEmpty() || Arrays.toString(checkPasswordField.getPassword()).isEmpty()) {
+                JOptionPane.showMessageDialog(this, "Please fill all fields", "Empty Fields", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            // Return if username is invalid
+            if (!Utilities.validateUsername(username)) {
+                JOptionPane.showMessageDialog(this, "Username must be between 3 and 20 characters", "Username Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            // Return if email is invalid
+            if (!Utilities.validateEmail(email)) {
+                JOptionPane.showMessageDialog(this, "Invalid email", "Email Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            // Return if date is in the future
+            if (((Date) dateSpinner.getValue()).compareTo(new Date()) > 0) {
+                JOptionPane.showMessageDialog(this, "Date of birth can't be in the future", "Date Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            // Return if password don't match, and initialize password if they do
             if (Arrays.toString(passwordField.getPassword()).equals(Arrays.toString(checkPasswordField.getPassword()))){
                 password = new String(passwordField.getPassword());
             }
@@ -53,23 +84,21 @@ public class SignUp extends JFrame{
                 return;
             }
 
+            // Backend validations
+
             // Return if username or email already exists
             // TODO: Implement this
 
-            Date dateOfBirth = (Date) dateSpinner.getValue();
-            if (Utilities.validateEmail(email) && Utilities.validateUsername(username)){
-                User user = new User(username, email, password, dateOfBirth);
+            // Would only reach here if all fields are valid
+            User user = new User(username, email, password, dateOfBirth);
 
-                // TODO: Add user to database
+            // TODO: Add user to database
 
-                JOptionPane.showMessageDialog(this, "New user created", "backend.User Created", JOptionPane.INFORMATION_MESSAGE);
-                System.out.println(user.getUserId());
+            JOptionPane.showMessageDialog(this, "New user created", "User Created", JOptionPane.INFORMATION_MESSAGE);
+            System.out.println(user.getUserId());
 
-                // TODO: Open new user's profile
-            }
-            else{
-                JOptionPane.showMessageDialog(this, "You entered wrong data", "Wrong Input", JOptionPane.ERROR_MESSAGE);
-            }
+            // TODO: Open new user's profile
+            // Profile window pending
         });
 
         backButton.addActionListener(_ -> {
