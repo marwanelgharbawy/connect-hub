3d48b828b49697d09171210586dfbe5773892c00
Habiba <habibaswaify@hotmail.com>
2024-12-04 02:32:08 +0200

    Add FriendRequest.java

---
+++ src/FriendRequest.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+
+public class FriendRequest {
+    private final User sender; // User who sent the friend request
+    private final User receiver; // User who is receiving the friend request
+    private String status; // Status of the friend request : pending, accepted, declined
+    private final LocalDate timestamp; // The date on which the friend request was sent
+
+    public FriendRequest(User sender, User receiver) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.status = "pending";
+        this.timestamp = LocalDate.now();
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public LocalDate getTimestamp() {
+        return timestamp;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+}
