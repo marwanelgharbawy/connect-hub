04eba3547e036a9cdf78dddf7edcca149676385c
Omar Hekal <omar.abdeldayem23@gmail.com>
2024-12-04 22:12:13 +0200

    fix bug in isDue func

--- src/content/Story.java
+++ src/content/Story.java
@@ -1,6 +1,7 @@
 package content;
 import org.json.JSONObject;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class Story extends CreatedContent{
@@ -9,7 +10,10 @@ public class Story extends CreatedContent{
     }
     // Check if the story is due in order to delete it
     boolean isDue(){
-        return (LocalDateTime.now().getHour() - super.timestamp.getHour() >= 24);
+//        return (LocalDateTime.now().getHour() - super.timestamp.getHour() >= 24);
+        LocalDateTime now = LocalDateTime.now();
+        Duration duration = Duration.between(super.timestamp, now);
+        return duration.toDays() > 0;
     }
 
     @Override
