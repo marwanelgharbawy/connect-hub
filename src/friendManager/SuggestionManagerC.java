df75b385b37e9bb9be08e36bbdcb2b8355473e92
Habiba <habibaswaify@hotmail.com>
2024-12-04 06:28:06 +0200

    Added SuggestionManager

---
+++ src/friendManager/SuggestionManagerC.java
@@ -0,0 +1,22 @@
+package friendManager;
+
+import user.User;
+
+import java.util.ArrayList;
+
+public class SuggestionManagerC implements SuggestionManagerI {
+    @Override
+    public ArrayList<User> suggestFriends(User user) {
+        ArrayList<User> suggestions = new ArrayList<>();
+        // Suggest friends of friends, handle the case if the friends are mutual or finding the main user
+        for (User friend : user.getFriendManager().getFriends()) {
+            for (User friendOfFriend : friend.getFriendManager().getFriends()) {
+                if (!user.getFriendManager().getFriends().contains(friendOfFriend)
+                        && !friendOfFriend.equals(user)) {
+                    suggestions.add(friendOfFriend);
+                }
+            }
+        }
+        return suggestions;
+    }
+}
