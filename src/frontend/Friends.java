207422a87a7080919e31e7e6a990269547fc3aa1
Habiba <habibaswaify@hotmail.com>
2024-12-05 04:12:28 +0200

    Modify Friends

--- src/frontend/Friends.java
+++ src/frontend/Friends.java
@@ -4,8 +4,6 @@ import backend.User;
 import utils.UIUtils;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Friends extends JFrame {
     private JComboBox<UserComboBoxItem> friendsComboBox;
@@ -13,43 +11,50 @@ public class Friends extends JFrame {
     private JButton blockButton;
     private JButton removeButton;
     private final User user;
-    Friends(User user){
+
+    Friends(User user) {
         this.user = user;
         UIUtils.initializeWindow(this, mainPanel, "Friends", 400, 400);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        removeButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Get friend from the combo box
-                UserComboBoxItem userComboBoxItem = (UserComboBoxItem) friendsComboBox.getSelectedItem();
-                // Remove friend
-                if (friendsComboBox.getSelectedItem()!=null){
-                    user.getFriendManager().removeFriend(user,((UserComboBoxItem) friendsComboBox.getSelectedItem()).getUser());
-                }
-                else {
-                    JOptionPane.showMessageDialog(null,"No friend selected!","Empty Field Error",JOptionPane.ERROR_MESSAGE);
-                }
-            }
-        });
-        blockButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Get friend from the combo box
-                UserComboBoxItem userComboBoxItem = (UserComboBoxItem) friendsComboBox.getSelectedItem();
-                // Block friend
-                if (friendsComboBox.getSelectedItem()!=null){
-                    user.getFriendManager().getBlockManager().blockUser(user,((UserComboBoxItem) friendsComboBox.getSelectedItem()).getUser());
-                }
-                else {
-                    JOptionPane.showMessageDialog(null,"No friend selected!","Empty Field Error",JOptionPane.ERROR_MESSAGE);
-                }
-
-            }
-        });
+        // Update combo box with user's friends
+        updateComboBox();
+        // Setup button listeners for friend actions
+        setupButtonListeners();
     }
-    void populateComboBox(){
-        for(User friend : user.getFriendManager().getFriends()){
-            friendsComboBox.addItem(new UserComboBoxItem(user));
+
+    private void setupButtonListeners() {
+        // Remove friend
+        removeButton.addActionListener(_ -> {
+            if (friendsComboBox.getSelectedItem() != null) {
+                user.getFriendManager().removeFriend(user, ((UserComboBoxItem) friendsComboBox.getSelectedItem()).getUser());
+                // Update combo box after each deletion
+                updateComboBox();
+            } else {
+                // Display an error message if no friend is selected
+                JOptionPane.showMessageDialog(null, "No friend selected!", "Empty Field Error", JOptionPane.ERROR_MESSAGE);
+            }
+        });
+        // Block friend
+        blockButton.addActionListener(_ -> {
+            if (friendsComboBox.getSelectedItem() != null) {
+                user.getFriendManager().getBlockManager().blockUser(user, ((UserComboBoxItem) friendsComboBox.getSelectedItem()).getUser());
+                // Update combo box after each blocking
+                updateComboBox();
+            } else {
+                // Display an error ,essage if no friend is selected
+                JOptionPane.showMessageDialog(null, "No friend selected!", "Empty Field Error", JOptionPane.ERROR_MESSAGE);
+            }
+
+        });
+
+    }
+
+    void updateComboBox() {
+        // Clear all items in the combo box
+        friendsComboBox.removeAllItems();
+        for (User friend : user.getFriendManager().getFriends()) {
+            // Add each friend to the combo box
+            friendsComboBox.addItem(new UserComboBoxItem(friend));
         }
 
     }
