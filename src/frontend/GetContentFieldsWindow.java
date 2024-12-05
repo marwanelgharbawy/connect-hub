d089df024d0bd1f1958146542742f2a1c7b3a9b4
Habiba <habibaswaify@hotmail.com>
2024-12-05 04:11:49 +0200

    Modify GetContentFieldsWindow

--- src/frontend/GetContentFieldsWindow.java
+++ src/frontend/GetContentFieldsWindow.java
@@ -4,11 +4,9 @@ import content.ContentFields;
 import utils.UIUtils;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 
-public class GetContentFieldsWindow extends JFrame{
+public class GetContentFieldsWindow extends JFrame {
     private JTextField textField1; //Input text field
     private JPanel mainPanel;
     private JButton addPictureButton; // Button to upload picture
@@ -16,37 +14,42 @@ public class GetContentFieldsWindow extends JFrame{
     String text;
     String imagePath;
     String title;
+
     public GetContentFieldsWindow(ContentFields contentFields, boolean isStory) {
-        if(isStory)
+        // Set the title dynamically according to the instance of the content
+        if (isStory) {
             title = "Add Story";
-        else title = "Add Post";
+        } else {
+            title = "Add Post";
+        }
         UIUtils.initializeWindow(this, mainPanel, title, 400, 400);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        // Get picture path
-        addPictureButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                imagePath = addPicture();
-            }
-        });
-        submitButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Get text and check that the field is not empty
-                text = textField1.getText();
-                if(!text.isEmpty()){
-                    contentFields.setText(text);
-                    contentFields.setImagePath(imagePath);
-                    dispose();
-                }
-                else {
-                    JOptionPane.showMessageDialog(null,"Text field is empty!","Empty Field Error",JOptionPane.ERROR_MESSAGE);
-                }
+        // Set up the button listeners for content parsing
+        setupButtonListeners(contentFields);
+    }
+
+    // Set up the button action listeners
+    private void setupButtonListeners(ContentFields contentFields) {
+
+        // Get picture path and add picture to project directory
+        addPictureButton.addActionListener(_ -> imagePath = addPicture());
+        // Get content
+        submitButton.addActionListener(_ -> {
+            // Get text and check that the field is not empty
+            text = textField1.getText();
+            if (!text.isEmpty()) {
+                contentFields.setText(text);
+                contentFields.setImagePath(imagePath);
+                dispose();
+            } else {
+                // Display an error message if text field is empty
+                JOptionPane.showMessageDialog(null, "Text field is empty!", "Empty Field Error", JOptionPane.ERROR_MESSAGE);
             }
         });
 
     }
-    private String addPicture(){
+
+    private String addPicture() {
         // Get file via file chooser
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setDialogTitle("Upload Image");
@@ -60,12 +63,15 @@ public class GetContentFieldsWindow extends JFrame{
             // Check if the file is in the correct format : ".png",".jpg",".jpeg"
             if (!file.getName().toLowerCase().endsWith(".png") && !file.getName().toLowerCase().endsWith(".jpg")
                     && !file.getName().toLowerCase().endsWith(".jpeg")) {
+                // Display error message if format is invalid
                 JOptionPane.showMessageDialog(null, "Invalid file format! Please an image file.", "Error", JOptionPane.ERROR_MESSAGE);
                 return null;
             }
+            // Return image path
             System.out.println(file.getPath());
             return file.getPath();
         }
+        // Return null if no image is selected
         return null;
     }
 }
