a6e91d41ab8eb9c8eda61f90e29d5a6fb44a5bd0
Habiba <habibaswaify@hotmail.com>
2024-12-06 00:43:06 +0200

    Modify SuggestionManager

--- src/friendManager/SuggestionManagerI.java
+++ src/friendManager/SuggestionManagerI.java
@@ -6,10 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public interface SuggestionManagerI {
-    void suggestFriend(User friend, User suggestedFriend) throws IOException;
-    public ArrayList<User> getFriendsOfFriends(User user);
-    ArrayList<User> getSuggestions();
+    ArrayList<User> getFriendsOfFriends(User user);
     void removeSuggestion(User user);
-    void addSuggestion(User user);
     void refuseSuggestion(User mainUser,User user) throws IOException;
 }
