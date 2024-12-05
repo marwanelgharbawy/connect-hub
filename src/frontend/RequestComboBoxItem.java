2827a3bbf6f973a5a26107263c3f41ae1a907900
Habiba <habibaswaify@hotmail.com>
2024-12-05 04:11:23 +0200

    Add RequestComboBoxItem

---
+++ src/frontend/RequestComboBoxItem.java
@@ -0,0 +1,24 @@
+package frontend;
+
+import backend.User;
+import friendManager.FriendRequest;
+
+public class RequestComboBoxItem {
+    private final FriendRequest friendRequest;
+
+    // Constructor
+    public RequestComboBoxItem(FriendRequest friendRequest) {
+        this.friendRequest = friendRequest;
+    }
+
+    // Getter
+    public FriendRequest getFriendRequest() {
+        return friendRequest;
+    }
+
+    // Override toString to return the display text
+    @Override
+    public String toString() {
+        return friendRequest.getSender().getUsername();
+    }
+}
