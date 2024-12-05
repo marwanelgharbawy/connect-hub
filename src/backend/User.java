3c44e3a8f6ece6a4da6aaf5314a5aa7d31e3a3c4
Habiba <habibaswaify@hotmail.com>
2024-12-05 17:52:57 +0200

    Modify User

--- src/backend/User.java
+++ src/backend/User.java
@@ -1,14 +1,12 @@
 package backend;
-import content.Post;
-import content.Story;
-import friendManager.FriendManagerC;
-import friendManager.FriendManagerFactory;
-import friendManager.FriendManagerI;
+
+import friendManager.*;
 
 import utils.Utilities;
 
 import java.io.IOException;
 import java.time.LocalDate;
+
 import org.json.*;
 
 public class User {
@@ -22,10 +20,11 @@ public class User {
     String profile_img_path;
     String cover_img_path;
 
-    public User(){
+    public User() {
         this.friendManager = FriendManagerFactory.createFriendManager();
     }
-    public User(String username, String email, String password, LocalDate dateOfBirth){
+
+    public User(String username, String email, String password, LocalDate dateOfBirth) {
         this.userId = Utilities.generateId();
         this.email = email;
         this.username = username;
@@ -35,7 +34,7 @@ public class User {
         this.friendManager = FriendManagerFactory.createFriendManager();
     }
 
-    public User(JSONObject credentials){
+    public User(JSONObject credentials) {
         userId = credentials.getString("id");
         username = credentials.getString("username");
         email = credentials.getString("email");
@@ -51,12 +50,14 @@ public class User {
         // TODO: profile management: posts, stories
 
         Database database = Database.getInstance();
-        JSONArray friends = userData.getJSONArray("friends");
-        for(Object friend: friends){
-            String friend_id = (String) friend;
-            User friend_ = database.getUser(friend_id);
-            friendManager.addFriend(friend_);
-        }
+        // friends
+        setFriends(database,userData);
+        // blocked
+        setBlocked(database,userData);
+        // requests
+        setRequests(database,userData);
+        // suggestions
+        setSuggestions(database,userData);
     }
 
     public boolean isOnline() {
@@ -99,7 +100,7 @@ public class User {
         this.username = username;
     }
 
-    public void setPassword(String password){
+    public void setPassword(String password) {
         this.password = Utilities.hashPassword(password);
     }
 
@@ -107,7 +108,7 @@ public class User {
         return friendManager;
     }
 
-    public JSONObject getCredentials(){
+    public JSONObject getCredentials() {
         JSONObject credentials = new JSONObject();
         credentials.put("id", userId);
         credentials.put("username", username);
@@ -116,21 +117,20 @@ public class User {
         return credentials;
     }
 
-    public JSONObject getUserData(){
+    public JSONObject getUserData() {
         JSONObject data = new JSONObject();
         data.put("dateOfBirth", Utilities.DateTo_y_M_d(dateOfBirth));
         data.put("online", online);
         data.put("profile-photo", profile_img_path);
         data.put("cover-photo", cover_img_path);
-
-
         /* friends */
-        JSONArray friends = new JSONArray();
-        for(User user: friendManager.getFriends()){
-            friends.put(user.getUserId());
-        }
-        data.put("friends", friends);
-
+        loadFriends(data);
+        /* blocked */
+        loadBlocked(data);
+        /* friend requests */
+        loadRequests(data);
+        /* suggestions */
+        loadSuggestions(data);
         /* posts */
         JSONArray posts = new JSONArray();
         // TODO: posts
@@ -147,8 +147,94 @@ public class User {
 //        }
         data.put("stories", stories);
 
-
         return data;
+    }
+    public void setFriends(Database database,JSONObject userData){
+        JSONArray friends = userData.getJSONArray("friends");
+        for (Object friend : friends) {
+            String friend_id = (String) friend;
+            User friend_ = database.getUser(friend_id);
+            if (friend_ != null) {
+                if (!FriendUtils.isDuplicate(friend_, friendManager.getFriends())) {
+                    friendManager.addFriend(friend_);
+                }
+            }
+        }
+
+    }
+    public void setBlocked(Database database,JSONObject userData){
+        JSONArray blocked = userData.getJSONArray("blocked");
+        for (Object blockedUser : blocked) {
+            String blockedUserId = (String) blockedUser;
+            User blockedUser_ = database.getUser(blockedUserId);
+            if (blockedUser_ != null) {
+                if (!FriendUtils.isDuplicate(blockedUser_, friendManager.getBlockManager().getBlockedUsers())) {
+                    friendManager.getBlockManager().blockUser(this, blockedUser_);
+                }
+            }
+        }
+    }
+    public void setRequests(Database database,JSONObject userData){
+        JSONArray requests = userData.getJSONArray("requests");
+        for (Object request : requests) {
+            String senderId = (String) request;
+            User sender = database.getUser(senderId);
+            {
+                if (sender != null) {
+                    FriendRequest friendRequest = friendManager.getRequestManager().getReceivedRequest(senderId);
+                    if (friendRequest == null) {
+                        friendManager.getRequestManager().addReceivedRequest(new FriendRequest(sender, this));
+                    }
+                }
+            }
+
+        }
+
+    }
+    public void setSuggestions(Database database,JSONObject userData){
+        JSONArray suggestions = userData.getJSONArray("suggestions");
+        for (Object suggested : suggestions) {
+            String suggestedId = (String) suggested;
+            User suggested_ = database.getUser(suggestedId);
+            if (suggested_ != null) {
+                if (!FriendUtils.isDuplicate(suggested_, friendManager.getSuggestionManager().getSuggestions())) {
+                    friendManager.getSuggestionManager().addSuggestion(suggested_);
+                }
+            }
+        }
+
+    }
+    public void loadFriends(JSONObject data){
+        JSONArray friends = new JSONArray();
+        for (User user : friendManager.getFriends()) {
+            friends.put(user.getUserId());
+        }
+        data.put("friends", friends);
+
+    }
+    public void loadBlocked(JSONObject data){
+        JSONArray blocked = new JSONArray();
+        for (User user : friendManager.getBlockManager().getBlockedUsers()) {
+            blocked.put(user.getUserId());
+        }
+        data.put("blocked", blocked);
+
+    }
+    public void loadRequests(JSONObject data){
+        JSONArray friendRequests = new JSONArray();
+        for (FriendRequest friendRequest : friendManager.getRequestManager().getReceivedRequests()) {
+            friendRequests.put(friendRequest.getReceiver().userId); // Each friend request is linked to its sender
+        }
+        data.put("requests", friendRequests);
+
+    }
+    public void loadSuggestions(JSONObject data){
+        JSONArray suggestions = new JSONArray();
+        for (User user : friendManager.getSuggestionManager().getSuggestions()) {
+            suggestions.put(user.getUserId());
+        }
+        data.put("suggestions", suggestions);
+
     }
 
 }
