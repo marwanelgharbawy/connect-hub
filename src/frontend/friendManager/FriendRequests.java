c8e166b67d7e644dffb310c047ab7d802a3bb315
Habiba <habibaswaify@hotmail.com>
2024-12-05 22:32:45 +0200

    Modify FriendRequests

--- src/frontend/friendManager/FriendRequests.java
+++ src/frontend/friendManager/FriendRequests.java
@@ -9,16 +9,14 @@ import javax.swing.*;
 import java.io.IOException;
 
 public class FriendRequests extends JFrame {
-    private JComboBox<FriendRequest> requestsComboBox;
+    private JComboBox<RequestComboBoxItem> requestsComboBox;
     private JPanel mainPanel;
     private JButton declineButton;
     private JButton acceptButton;
     private final User user;
-    private final Database database;
 
-    public FriendRequests(Database database,User user) {
+    public FriendRequests(User user) {
         this.user = user;
-        this.database = database;
         UIUtils.initializeWindow(this, mainPanel, "Friend Requests", 400, 400);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         // Update the combo box with friend requests
@@ -34,9 +32,13 @@ public class FriendRequests extends JFrame {
         acceptButton.addActionListener(_ -> {
             if (requestsComboBox.getSelectedItem() != null) {
                 // Call the friend manager and the request manager respectively to accept the request
-                user.getFriendManager().getRequestManager().acceptFriendRequest(((RequestComboBoxItem) requestsComboBox.getSelectedItem()).getFriendRequest());
-                // Update combo box and database after each addition
-                updateData();
+                try {
+                    user.getFriendManager().getRequestManager().acceptFriendRequest(((RequestComboBoxItem) requestsComboBox.getSelectedItem()).getFriendRequest());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                // Update combo box after each addition
+                updateComboBox();
             } else {
                 // Display error message if no item is selected
                 JOptionPane.showMessageDialog(null, "No request selected!", "Empty Field Error", JOptionPane.ERROR_MESSAGE);
@@ -47,9 +49,13 @@ public class FriendRequests extends JFrame {
         declineButton.addActionListener(_ -> {
             if (requestsComboBox.getSelectedItem() != null) {
                 // Call the friend manager and the request manager respectively to decline the request
-                user.getFriendManager().getRequestManager().cancelRequest(((RequestComboBoxItem) requestsComboBox.getSelectedItem()).getFriendRequest());
-                // Update combo box and database after each deletion
-                updateData();
+                try {
+                    user.getFriendManager().getRequestManager().cancelRequest(((RequestComboBoxItem) requestsComboBox.getSelectedItem()).getFriendRequest());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                // Update combo box after each deletion
+                updateComboBox();
             } else {
                 // Display error message if no item is selected
                 JOptionPane.showMessageDialog(null, "No request selected!", "Empty Field Error", JOptionPane.ERROR_MESSAGE);
@@ -58,21 +64,13 @@ public class FriendRequests extends JFrame {
         });
 
     }
-    void updateData(){
-        updateComboBox();
-        try {
-            database.saveUser(user);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
     // Update the friend requests combo box
     void updateComboBox() {
         // Clear all items in the combo box
         requestsComboBox.removeAllItems();
         // Add each request to the combo box
         for (FriendRequest friendRequest : user.getFriendManager().getRequestManager().getReceivedRequests()) {
-            requestsComboBox.addItem(friendRequest);
+            requestsComboBox.addItem(new RequestComboBoxItem(friendRequest));
         }
 
     }
