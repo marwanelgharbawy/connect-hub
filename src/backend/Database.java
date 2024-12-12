321ca55b00dbbf162a514e7b1875c45bd57aed86
Habiba <habibaswaify@hotmail.com>
2024-12-13 01:42:19 +0200

    Modify Database.java

--- src/backend/Database.java
+++ src/backend/Database.java
@@ -86,7 +86,7 @@ public class Database {
             User user = new User(jsonObject);
             id_to_user.put(user.getUserId(), user);
             email_to_user.put(user.getEmail(), user);
-            username_to_user.put(user.getEmail(), user);
+            username_to_user.put(user.getUsername(), user);
             System.out.println("Successfully added user: " + user.getUsername());
         }
 
