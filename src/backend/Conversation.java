9bf0fdf310b913f58c6bd6688d30c8499e84dfb2
Marwan <marwanelgharbawy2@gmail.com>
2024-12-16 17:12:43 +0200

    Modify conversation constructor to include a message list

--- src/backend/Conversation.java
+++ src/backend/Conversation.java
@@ -8,25 +8,14 @@ import java.util.ArrayList;
 public class Conversation {
     private final User user1;
     private final User user2;
-    private final ArrayList<Message> messages = new ArrayList<Message>();
+    private final ArrayList<Message> messages;
     private final Database database;
 
-    // Normal constructor
-    public Conversation(User user1, User user2) {
+    public Conversation(User user1, User user2, ArrayList<Message> messages) {
         this.user1 = user1;
         this.user2 = user2;
+        this.messages = messages;
         this.database = Database.getInstance();
-    }
-
-    // Load from database
-    public Conversation(JSONObject json) {
-        this.database = Database.getInstance();
-        this.user1 = database.getUser(json.getString("user1"));
-        this.user2 = database.getUser(json.getString("user2"));
-        JSONArray jsonMessages = json.getJSONArray("messages");
-        for (int i = 0; i < jsonMessages.length(); i++) {
-            this.messages.add(new Message(jsonMessages.getJSONObject(i)));
-        }
     }
 
     public User getUser1() {
