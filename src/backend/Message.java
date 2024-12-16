9376309e329adff324ae1a4e6b8b8ff7fedbacf0
Marwan <marwanelgharbawy2@gmail.com>
2024-12-16 16:07:27 +0200

    Finish Message class

--- src/backend/Message.java
+++ src/backend/Message.java
@@ -1,5 +1,6 @@
 package backend;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import org.json.*;
 
@@ -13,5 +14,25 @@ public class Message {
         this.content = message;
         this.sender = sender;
         this.timestamp = LocalDateTime.now();
+    }
+
+    // Load message from database
+    public Message(JSONObject json) throws IOException {
+        this.content = json.getString("content");
+        String UserID = json.getString("sender");
+        this.sender = Database.getInstance().getUser(UserID);
+        this.timestamp = LocalDateTime.parse(json.getString("timestamp"));
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
     }
 }
